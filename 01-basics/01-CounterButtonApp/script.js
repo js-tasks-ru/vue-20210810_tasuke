335a1be1710e441b8ec1e6475530fe9b0a7e5bcf
Алексеев Алексей <Alale20002504@gmail.com>
2021-08-18 08:17:58 +0500

    Completed CounterButton App and VueCalculator

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,17 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const app = {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    doIncrement() {
+      this.counter++;
+    },
+  },
+};
+
+createApp(app).mount('#app');

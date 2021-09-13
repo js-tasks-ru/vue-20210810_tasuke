6fbe6207e116d951991336843250891045d790e9
Алексеев Алексей <Alale20002504@gmail.com>
2021-09-13 08:44:26 +0500

    Completed Tasks: CounterButton, MeetupDescription

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -2,11 +2,18 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'CounterButton',
-
+  props: {
+    count: Number,
+  },
+  emits: ['update:count'],
   // Компонент должен иметь входной параметр
 
   // Шаблон лучше держать максимально простым, а логику выносить в методы
 
   // Шаблон потребуется отредактировать
-  template: `<button type="button">1</button>`,
+  template: `<button type="button"
+                     :value="Number(count) + Number(1)"
+                     @click="$emit('update:count', $event.target.value++)">
+              {{count}}
+            </button>`,
 });

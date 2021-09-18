589a3fcb0330fdcc0da128667603f12d12b99734
Алексеев Алексей <Alale20002504@gmail.com>
2021-09-18 15:56:14 +0500

    Complteted: MeetupCover and MeetupInfo

---
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -0,0 +1,33 @@
+import { defineComponent } from './vendor/vue.esm-browser.js';
+
+export default defineComponent({
+  name: 'MeetupInfo',
+
+  props: {
+    organizer: String,
+    place: String,
+    date: Number,
+  },
+
+  computed: {
+    localDate() {
+      return new Date(this.date).toLocaleDateString(navigator.language);
+    },
+  },
+
+  template: `
+    <ul class="meetup-info">
+      <li>
+        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
+        {{this.organizer}}
+      </li>
+      <li>
+        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
+        {{this.place}}
+      </li>
+      <li>
+        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
+        <time datetime="2020-01-01">{{this.localDate}}</time>
+      </li>
+    </ul>`,
+});
